e50cbbc54da4d2f8a32fd7f3c23d8cde6af8e40f
Tom <itam8829@gmail.com>
2025-07-06 14:44:20 +0900

    1.빌드 파일 추가

---
+++ Assets/Editor/BuildScript.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.IO;
+
+public class BuildScript
+{
+    public static void BuildAndroid()
+    {
+        //EditorUserBuildSettings.SwitchActiveBuildTarget(BuildTargetGroup.Android, BuildTarget.Android);
+
+        string[] scenes = {
+            "Assets/Scenes/LogoScene.unity",
+            "Assets/Scenes/MainScene.unity",
+            "Assets/Scenes/MoreGameScene.unity",
+            "Assets/Scenes/GameScene.unity"
+        };
+
+        string outputPath = "Builds/Android/MyGame.apk";  // 출력 위치
+
+        string directory = Path.GetDirectoryName(outputPath);
+        if (!Directory.Exists(directory))
+        {
+            Directory.CreateDirectory(directory);
+            Debug.Log("🔧 Build output directory created: " + directory);
+        }
+
+        EditorUserBuildSettings.buildAppBundle = false;
+
+        //키스토어 설정
+        PlayerSettings.Android.useCustomKeystore = true;
+        PlayerSettings.Android.keystoreName = "dancingkong.keystore"; // 키스토어 파일명
+        PlayerSettings.Android.keystorePass = "dancingkong";
+        PlayerSettings.Android.keyaliasName = "dancingkong";
+        PlayerSettings.Android.keyaliasPass = "dancingkong";
+
+        BuildPlayerOptions buildOptions = new BuildPlayerOptions
+        {
+            scenes = scenes,
+            locationPathName = outputPath,
+            target = BuildTarget.Android,
+            options = BuildOptions.None
+        };
+
+        BuildPipeline.BuildPlayer(buildOptions);
+    }
+}
